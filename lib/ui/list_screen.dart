6fe946676d38acdabdbba3c937e95a8dc6bc0c01
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-07-08 14:39:51 -0300

    Ordenando itens por comprado ou não

--- lib/ui/list_screen.dart
+++ lib/ui/list_screen.dart
@@ -19,36 +19,40 @@ class ListScreenState extends State<ListScreen> {
         title: const Text("Lista de Compras"),
         centerTitle: true,
       ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => ListTile(
-          leading: CircleAvatar(
-            child: IconTheme(
-              data: const IconThemeData(color: Colors.white),
-              child: Icon(
-                items[index].isDone ? Icons.check_circle : Icons.shopping_cart,
+      body: RefreshIndicator(
+          onRefresh: _refresh,
+          child: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+              leading: CircleAvatar(
+                child: IconTheme(
+                  data: const IconThemeData(color: Colors.white),
+                  child: Icon(
+                    items[index].isDone
+                        ? Icons.check_circle
+                        : Icons.shopping_cart,
+                  ),
+                ),
+              ),
+              title: Text(items[index].title),
+              onTap: () {
+                setState(() {
+                  items[index].isDone = !items[index].isDone;
+                });
+              },
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    items.removeAt(index);
+                  });
+                },
               ),
             ),
-          ),
-          title: Text(items[index].title),
-          onTap: () {
-            setState(() {
-              items[index].isDone = !items[index].isDone;
-            });
-          },
-          trailing: IconButton(
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              setState(() {
-                items.removeAt(index);
-              });
-            },
-          ),
-        ),
-        separatorBuilder: (context, index) => const Divider(
-          color: Colors.orange,
-        ),
-        itemCount: items.length,
-      ),
+            separatorBuilder: (context, index) => const Divider(
+              color: Colors.orange,
+            ),
+            itemCount: items.length,
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await _addItem(context);
@@ -68,5 +72,22 @@ class ListScreenState extends State<ListScreen> {
         items.add(item);
       });
     }
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {
+      items.sort((a, b) {
+        if (a.isDone && !b.isDone) {
+          return 1;
+        } else if (!a.isDone && b.isDone) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    });
+
+    return Future.value();
   }
 }
