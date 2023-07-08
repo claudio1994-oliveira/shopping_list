e0440657460f517c21ee38abc9356d5a877cdd05
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-07-08 14:27:12 -0300

    Marcando item como 'comprado'

--- lib/ui/list_screen.dart
+++ lib/ui/list_screen.dart
@@ -4,17 +4,14 @@ import 'package:lista_compras/ui/add_item.dart';
 
 class ListScreen extends StatefulWidget {
   const ListScreen({Key? key}) : super(key: key);
+
   @override
   State<ListScreen> createState() => ListScreenState();
 }
 
 class ListScreenState extends State<ListScreen> {
-  List<Item> items = <Item>[
-    Item(title: 'teste1', isDone: false),
-    Item(title: 'teste2', isDone: false),
-    Item(title: 'teste3', isDone: false),
-    Item(title: 'teste4', isDone: false),
-  ];
+  List<Item> items = <Item>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,40 +20,53 @@ class ListScreenState extends State<ListScreen> {
         centerTitle: true,
       ),
       body: ListView.separated(
-          itemBuilder: ((context, index) => ListTile(
-                leading: const CircleAvatar(
-                  child: IconTheme(
-                      data: IconThemeData(color: Colors.white),
-                      child: Icon(Icons.shopping_cart)),
-                ),
-                title: Text(items[index].title),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    setState(() {
-                      items.removeAt(index);
-                    });
-                  },
-                ),
-              )),
-          separatorBuilder: (context, index) => const Divider(
-                color: Colors.orange,
+        itemBuilder: (context, index) => ListTile(
+          leading: CircleAvatar(
+            child: IconTheme(
+              data: const IconThemeData(color: Colors.white),
+              child: Icon(
+                items[index].isDone ? Icons.check_circle : Icons.shopping_cart,
               ),
-          itemCount: items.length),
+            ),
+          ),
+          title: Text(items[index].title),
+          onTap: () {
+            setState(() {
+              items[index].isDone = !items[index].isDone;
+            });
+          },
+          trailing: IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              setState(() {
+                items.removeAt(index);
+              });
+            },
+          ),
+        ),
+        separatorBuilder: (context, index) => const Divider(
+          color: Colors.orange,
+        ),
+        itemCount: items.length,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          _addItem(context);
+          await _addItem(context);
         },
         child: const Icon(Icons.add),
       ),
     );
   }
 
-  void _addItem(BuildContext context) async {
+  Future<void> _addItem(BuildContext context) async {
     final item = await showDialog<Item>(
-        context: context, builder: (BuildContext context) => AddItem());
-    setState(() {
-      items.add(item!);
-    });
+      context: context,
+      builder: (BuildContext context) => AddItem(),
+    );
+    if (item != null) {
+      setState(() {
+        items.add(item);
+      });
+    }
   }
 }
